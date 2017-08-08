4f3d3ec5836277e64aeb9a5440fee10c7833ece3
Howard Feger <hfeger4@gmail.com>
2017-08-08 10:19:41 -0700

    Todo list view

--- auth-master/app/components/TodoList.js
+++ auth-master/app/components/TodoList.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/Octicons';
+import {connect} from 'react-redux';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView,
+  RefreshControl
 } from 'react-native';
 
+var TodoItem = React.createClass({
+  render(){
+    return(
+      <View style={styles.todoContainer}>
+        <Text>{this.props.text}</Text>
+      </View>
+    );
+  }
+});
+
 var TodoList = React.createClass({
+  getInitialState(){
+    return {
+      refreshing: false
+    };
+  },
   onLogout(){
 
   },
   addNewTodo(){
 
   },
+  onRefresh(){
+
+  },
   render() {
+    console.log("todos", this.props.todos);
+    var renderTodos = () => {
+      return this.props.todos.map((todo) => {
+        return (
+          <TodoItem key={todo._id} text={todo.text} id={todo._id}/>
+        );
+      });
+    };
     return (
       <View style={styles.container}>
         <View style={styles.topBar}>
@@ -29,6 +58,16 @@ var TodoList = React.createClass({
             <Icon name='plus' size={20} color='white'/>
           </TouchableOpacity>
         </View>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this.onRefresh}/>
+          }
+          automaticallyAdjustContentInsets={false}
+          contentContainerStyle={styles.scrollViewContainer}>
+          {renderTodos()}
+          </ScrollView>
       </View>
     );
   }
@@ -52,7 +91,20 @@ const styles = StyleSheet.create({
   title:{
     color: 'white',
     fontSize: 20
+  },
+  todoContainer: {
+    padding: 16,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    marginTop: -1,
+    borderColor: "#ccc"
   }
 });
 
-module.exports = TodoList;
+var mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+
+module.exports = connect(mapStateToProps)(TodoList);
