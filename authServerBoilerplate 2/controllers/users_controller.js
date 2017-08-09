e2576b5a85c42d7bc813bbc266986650bc92f680
joeyjira <joey.jira@gmail.com>
2017-08-09 10:45:49 -0700

    user index working

--- authServerBoilerplate 2/controllers/users_controller.js
+++ authServerBoilerplate 2/controllers/users_controller.js
@@ -2,13 +2,14 @@ const User = require("../models/user.js");
 
 module.exports = {
   getUsers(req, res, next) {
-    User.find({}).then(users => res.send(users)).catch(next());
+    
+    User.find({}).then(users => res.send(users)).catch(next);
   },
 
   getUser(req, res, next) {
     const userId = req.params.id;
 
-    User.findOne({ _id: userId }).then(user => res.send(user)).catch(next());
+    User.findOne({ _id: userId }).then(user => res.send(user)).catch(next);
   },
 
   updateUser(req, res, next) {}
