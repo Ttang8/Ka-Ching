9ec6552a99ec0fc09ccb791dbfe3b215b9a951e1
Terrence Tang <thtang28@gmail.com>
2017-08-09 09:55:18 -0700

    Add index to items controller and add errors middleware to index.js

--- authServerBoilerplate 2/controllers/items_controller.js
+++ authServerBoilerplate 2/controllers/items_controller.js
@@ -1,5 +1,14 @@
 const Item = require('../models/item');
 
 module.exports = {
+  index(req, res, next) {
+    const { lng, lat } = req.query;
 
-}
+    Item.geoNear(
+      { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)]},
+      { spherical: true, maxDistance: 8000}
+    )
+      .then(items => res.send(items))
+      .catch(next);
+  }
+};
