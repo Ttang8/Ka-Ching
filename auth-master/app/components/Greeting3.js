5ddad69902a66b9720fbb7d32fbacf73fe5ff749
Howard Feger <hfeger4@gmail.com>
2017-08-10 11:16:39 -0700

    add swiping and css change to login

---
+++ auth-master/app/components/Greeting3.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+
+const Greeting3 = () => {
+  const { textStyle, viewStyle, photo } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Everything is within 5 miles!</Text>
+      <Image
+        style={photo}
+        source={require('../images/exchange.jpg')}
+      />
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 340,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: .2,
+    elevation: 2,
+    position: 'relative',
+    },
+  textStyle: {
+    fontSize: 18,
+    padding: 15,
+    alignSelf: 'center',
+    justifyContent: 'center'
+
+  },
+  photo: {
+    height: 250,
+    width: 200,
+    borderRadius: 10
+  }
+};
+
+export default Greeting3;
