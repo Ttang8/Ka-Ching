95c777df70348faf5f6e2aa76ff43ccd6c849203
joeyjira <joey.jira@gmail.com>
2017-08-10 16:36:39 -0700

    added category routes

--- authServerBoilerplate 2/controllers/items_controller.js
+++ authServerBoilerplate 2/controllers/items_controller.js
@@ -1,13 +1,12 @@
-const Item = require('../models/item');
+const Item = require("../models/item");
 
 module.exports = {
-
   indexItems(req, res, next) {
     const { lng, lat } = req.query;
 
     Item.geoNear(
-      { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)]},
-      { spherical: true, maxDistance: 8000}
+      { type: "Point", coordinates: [parseFloat(lng), parseFloat(lat)] },
+      { spherical: true, maxDistance: 8000 }
     )
       .then(items => {
         res.send(items);
@@ -18,34 +17,39 @@ module.exports = {
   indexItem(req, res, next) {
     const itemId = req.params.id;
 
-    Item.findOne({ _id: itemId })
-      .then(user => res.send(user))
+    Item.findOne({ _id: itemId }).then(user => res.send(user)).catch(next);
+  },
+
+  categorizeItem(req, res, next) {
+    const itemCategory = req.params.category;
+    console.log("hiasdfasdfasfd")
+
+    Item.find({category: itemCategory})
+      .then(items => res.send(items))
       .catch(next);
   },
 
   createItem(req, res, next) {
     const itemProps = req.body;
 
-    Item.create(itemProps)
-      .then((item) => res.send(item))
-      .catch(next);
+    Item.create(itemProps).then(item => res.send(item)).catch(next);
   },
 
   editItem(req, res, next) {
     const itemId = req.params.id;
     const itemProps = req.body;
 
-    Item.findByIdAndUpdate({ _id: itemId}, itemProps)
-      .then(() => Item.findById({ _id: itemId}))
-      .then((item) => res.send(item))
+    Item.findByIdAndUpdate({ _id: itemId }, itemProps)
+      .then(() => Item.findById({ _id: itemId }))
+      .then(item => res.send(item))
       .catch(next);
   },
 
   deleteItem(req, res, next) {
     const itemId = req.params.id;
 
-    Item.findByIdAndRemove({ _id: itemId})
-      .then((item) => res.status(204).send(item))
+    Item.findByIdAndRemove({ _id: itemId })
+      .then(item => res.status(204).send(item))
       .catch(next);
   }
 };
