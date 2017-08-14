c3c2292e2f968eeb85fb9011ed5b33f4d204996a
Terrence Tang <thtang28@gmail.com>
2017-08-13 21:11:42 -0700

    Update items.js

--- auth-master/app/components/item/items.js
+++ auth-master/app/components/item/items.js
@@ -65,7 +65,7 @@ class Items extends Component {
   }
 
   renderItemList() {
-    if (this.state.items === undefined || this.state.items === 0) {
+    if (this.state.items === undefined || this.state.items.length === 0) {
       return (
         <View>
           <Text>
