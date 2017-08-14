c1978c18f52892fde9651a12837e7dcd2dd0e52b
Howard Feger <hfeger4@gmail.com>
2017-08-13 17:20:05 -0700

    CSS item upload

--- auth-master/app/components/photo/ItemSubmitForm.js
+++ auth-master/app/components/photo/ItemSubmitForm.js
@@ -30,31 +30,48 @@ class ItemSubmitForm extends Component {
     const {title, description, price} = this.state.itemInfo;
 
     return (
-      <View>
-        <MapView initialRegion={{
-          latitude: this.props.navigation.state.params.itemInfo.region.latitude,
-          longitude: this.props.navigation.state.params.itemInfo.region.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }} style={styles.map}>
-          <MapView.Marker coordinate={{
+      <View style={styles.container}>
+        <View>
+          <View style={styles.paddingTop}>
+            <Text style={styles.title}>Item Summary:</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Title:&nbsp;{title}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Description:&nbsp;{description}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Price:&nbsp;{price}</Text>
+          </View>
+          <View style={styles.padding}>
+
+          </View>
+        </View>
+        <View style={styles.center}>
+          <Text style={styles.title}>
+            Location
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <MapView initialRegion={{
             latitude: this.props.navigation.state.params.itemInfo.region.latitude,
-            longitude: this.props.navigation.state.params.itemInfo.region.longitude
-          }}/>
-        </MapView>
-        <View>
-          <Text>{title}</Text>
-        </View>
-        <View>
-          <Text>{description}</Text>
-        </View>
-        <View>
-          <Text>{price}</Text>
+            longitude: this.props.navigation.state.params.itemInfo.region.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }} style={styles.map}>
+            <MapView.Marker coordinate={{
+              latitude: this.props.navigation.state.params.itemInfo.region.latitude,
+              longitude: this.props.navigation.state.params.itemInfo.region.longitude
+            }}/>
+          </MapView>
         </View>
         <TouchableOpacity onPress={this.handleSubmit}>
-          <Text>
+          <View style={styles.center}>
+          <Text style={styles.button}>
             Submit
           </Text>
+          </View>
         </TouchableOpacity>
       </View>
     );
@@ -64,14 +81,73 @@ class ItemSubmitForm extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "stretch"
+    backgroundColor: '#2ecc71'
   },
   map: {
     position: "absolute",
     flex: 1,
-    width: 300,
-    height: 300
+    width: 500,
+    height: 300,
+    paddingLeft: 50
+  },
+  center:{
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  topBar: {
+    padding: 16,
+    paddingTop: 28,
+    paddingBottom: 8,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#2ecc71'
+  },
+  title: {
+    fontSize: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    paddingBottom: 5
+  },
+  field: {
+    borderRadius: 5,
+    padding: 10,
+    paddingLeft: 8,
+    margin: 7,
+    marginTop: 10,
+    backgroundColor: 'white'
+  },
+  textInput: {
+    height: 26
+  },
+  buttonContainer: {
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 30,
+    color: 'white',
+    padding: 10
+  },
+  buttonLocation: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
+  },
+  padding: {
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  paddingTop: {
+    paddingTop: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
